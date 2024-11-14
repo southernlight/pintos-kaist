26491cc7333e37025274daed1a28427997e31c6c
southernlight <southernlight3816@gmail.com>
2024-11-14 14:31:33 +0900

    feat: add infinite loop in process_wait

--- userprog/process.c
+++ userprog/process.c
@@ -208,6 +208,9 @@ int process_wait(tid_t child_tid UNUSED) {
   /* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
    * XXX:       to add infinite loop here before
    * XXX:       implementing the process_wait. */
+
+  for (int i = 0; i < 100000000; i++) {
+  }
   return -1;
 }
 
